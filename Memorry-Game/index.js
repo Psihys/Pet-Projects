31279369509abbf25624ac4843ed7138c4317234
Bohdan Shevchenko <bogdan.s.0474@gmail.com>
2025-11-03 12:07:37 +0100

    clean folder

--- Memorry-Game/index.js
+++
@@ -1,7 +0,0 @@
-const item = document.getElementsByClassName("item");
-const startGame = document.getElementsByClassName("start");
-const array = document.getElementsByClassName("container")
-
-startGame.addEventListener("click", () =>{
-    
-})
